9e9a99d498c5c537b97246665b3659adc43c8e7c
lingwndr <chandracar@gmail.com>
2024-07-05 21:54:27 +0330

    change repo file structure

--- app/server.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-# cmd : flookup -S -A 127.0.0.1 ../1standard.fst
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-def analyze(word):
-	sock.sendto(bytes(word, "utf-8"), ("127.0.0.1", 6062))
-	data, addr = sock.recvfrom(6062)
-	return str(data, encoding="utf-8")
