33fc9118db1ea2a4eb5b2920b24be762b79c767f
lingwndr <chandracar@gmail.com>
2024-07-07 18:22:57 +0330

    build: changed classifiers, added long readme desc and license

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+this_directory = Path(__file__).parent
 
-VERSION = '2024.7.5' 
-DESCRIPTION = 'Persian word analyzer'
-LONG_DESCRIPTION = 'Inflectional anlyzer for contemporary Persian'
+VERSION = '2024.7.7' 
+DESCRIPTION = 'Contemporary Persian word analyzer'
+LONG_DESCRIPTION = (this_directory / "README.md").read_text(encoding='utf-8')
 
 # Setting up
 setup(
@@ -13,16 +15,18 @@ setup(
         author_email="<chandracar@gmail.com>",
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
+        long_description_content_type='text/markdown',
         packages=find_packages(),
         #package_dir={"cpia": "fsts/*.fst"},
         package_data={"cpia": ["fsts/*.fst", "streats/*"]},
         install_requires=['fst-lookup>=2024.7.3'], # add any additional packages that 
         # needs to be installed along with your package. Eg: 'caer'
         
-        keywords=['Persian', 'Farsi', 'Inflection', 'Analyzer'],
+        keywords=['Persian', 'Farsi', 'Inflection', 'Analyzer', 'Inormal', 'Formal', 'Lemmatizer', 'Converter'],
         classifiers= [
             "Intended Audience :: Developers",
             "Intended Audience :: Science/Research",
+            "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)","
             "Programming Language :: Python :: 3.8",
             "Programming Language :: Python :: 3.9",
             "Programming Language :: Python :: 3.10",
@@ -32,7 +36,6 @@ setup(
             "Topic :: Scientific/Engineering",
             "Topic :: Text Processing :: Linguistic",
             "Operating System :: Microsoft :: Windows",
-            "Operating System :: POSIX",
             "Operating System :: Unix",
             "Operating System :: MacOS",
         ]
